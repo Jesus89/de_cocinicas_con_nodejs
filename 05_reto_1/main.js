befbc4098d6ea904a472dc4df9d08bdc4e91fdf7
Jesús Arroyo Torrens <jesus.arroyo@bq.com>
2016-01-21 10:59:30 +0100

    Ejercicio 05

--- 05_reto_1/main.js
+++ 05_reto_1/main.js
@@ -1,1 +1,8 @@
 fs = require('fs');
+
+fs.readFile('./comida.txt', 'utf8', function (err,data) {
+  var arr = data.split("\n");
+  for (var i = 0; i < arr.length - 1; i++) {
+      console.log(arr[i] + " me mola");
+  };
+});
